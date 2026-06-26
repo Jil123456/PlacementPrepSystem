aa4473c022db9e94b24b53f14a969ef1e1709382
Jil Patel <jilpatel0310@gmail.com>
2026-06-26 21:04:14 +0530

    Fix seed timeout

--- server/server.js
+++ server/server.js
@@ -13,22 +13,27 @@ async function startServer() {
     await db.sequelize.sync();
     console.log('Database synced');
     
-    // Auto-seed if database is empty (First time production boot)
-    const questionCount = await db.Question.count();
-    if (questionCount === 0) {
-      console.log('Database is empty! Auto-running seeders...');
-      await require('./seeders/companyModeSeeder')(db);
-      await require('./seeders/roadmapSeeder')(db);
-      await require('./seeders/dsaSeeder')(db);
-      await require('./seeders/aptitudeSeeder')(db);
-      await require('./seeders/coreSubjectSeeder')(db);
-      await require('./seeders/hrSeeder')(db);
-      await require('./seeders/taskSeeder')(db);
-      console.log('Auto-seeding complete! Database is now fully populated.');
-    }
-
     app.listen(PORT, () => {
       console.log(`Server is running on port ${PORT}`);
+      
+      // Run auto-seed in the background so Render doesn't time out waiting for the port
+      db.Question.count().then(async (questionCount) => {
+        if (questionCount === 0) {
+          console.log('Database is empty! Auto-running seeders in background...');
+          try {
+            await require('./seeders/companyModeSeeder')(db);
+            await require('./seeders/roadmapSeeder')(db);
+            await require('./seeders/dsaSeeder')(db);
+            await require('./seeders/aptitudeSeeder')(db);
+            await require('./seeders/coreSubjectSeeder')(db);
+            await require('./seeders/hrSeeder')(db);
+            await require('./seeders/taskSeeder')(db);
+            console.log('Auto-seeding complete! Database is now fully populated.');
+          } catch (error) {
+            console.error('Seeding error:', error);
+          }
+        }
+      }).catch(err => console.error(err));
     });
   } catch (error) {
     console.error('Unable to connect to the database:', error);
