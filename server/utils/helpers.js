16f167ca0fb055ed6a31e011b96441a5dbec7157
Jil Patel <jilpatel0310@gmail.com>
2026-06-29 12:00:13 +0530

    Optimize login speed

--- server/utils/helpers.js
+++ server/utils/helpers.js
@@ -15,7 +15,7 @@ function generateToken(userId) {
  * Hash a plain-text password.
  */
 async function hashPassword(password) {
-  const salt = await bcrypt.genSalt(12);
+  const salt = await bcrypt.genSalt(10);
   return bcrypt.hash(password, salt);
 }
 
